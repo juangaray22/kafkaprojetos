db97cc2b29b7127ad78a018a898b4b11f0286b34
juangaray22 <juangaray22@gmail.com>
2020-04-05 15:51:52 +0100

    vendas e processamento kafka

---
+++ venda-ingressos/src/main/java/serializer/VendaSerializer.java
@@ -0,0 +1,21 @@
+package serializer;
+
+import org.apache.kafka.common.serialization.Serializer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import model.Venda;
+
+public class VendaSerializer implements Serializer<Venda>{
+
+	public byte[] serialize(String topic, Venda venda) {
+		try {
+			return new ObjectMapper().writeValueAsBytes(venda);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
